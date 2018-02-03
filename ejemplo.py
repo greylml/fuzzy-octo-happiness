f4c47d92ac909e5c5cc41389c2c85f1f0ef5c366
José-PC\José <vlogvilog3000@gmail.com>
2018-02-03 17:07:20 -0430

    Proyecto 1 para utopian

---
+++ ejemplo.py
@@ -0,0 +1,12 @@
+class Ejemplo:
+	"""Ejemplo de POO para steemit
+	clases, objetos y metodos"""
+	entero = 1234
+	def mensaje(self,msj):
+		print(msj)
+
+utopian = Ejemplo()
+grey = Ejemplo()
+
+print(utopian.entero)
+print(grey.mensaje("Hola utopian, saludos desde Venezuela"))
